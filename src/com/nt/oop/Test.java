9f63e10f8c4f3cf33be4cd968f15a58eb219a5b2
HP <HP@DESKTOP-0EU8NA1>
2020-01-06 16:07:37 +0530

    first commit.....

---
+++ src/com/nt/oop/Test.java
@@ -0,0 +1,17 @@
+package com.nt.oop;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i=10;
+		for( i=0;i<=10;i++) {
+			System.out.println("print number:"+i+"");
+			
+		}
+		System.out.println();
+
+	}
+
+}
